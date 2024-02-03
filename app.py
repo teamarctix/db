182ddeb3c1021e1c355d48580f10f9dedbb13f5d
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-02-03 21:41:32 +0530

    add mongo db support

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
+client = MongoClient('mongodb+srv://abcd:abcd@personal.dzhmvck.mongodb.net/?retryWrites=true&w=majority')
+db = client['data_db']
 
-class UserInput(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    api_id = db.Column(db.String(255), nullable=False)
-    api_hash = db.Column(db.String(255), nullable=False)
-    bot_token = db.Column(db.String(255), nullable=False)
+class UserInput:
+    def __init__(self, api_id, api_hash, bot_token):
+        self.api_id = api_id
+        self.api_hash = api_hash
+        self.bot_token = bot_token
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,13 +19,16 @@ def index():
         bot_token = request.form['bot_token']
 
         user_input = UserInput(api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-        db.session.add(user_input)
-        db.session.commit()
+        db.user_inputs.insert_one({
+            'api_id': user_input.api_id,
+            'api_hash': user_input.api_hash,
+            'bot_token': user_input.bot_token
+        })
 
         # Redirect to avoid form resubmission on page refresh
         return redirect(url_for('index'))
 
-    inputs = UserInput.query.all()
+    inputs = db.user_inputs.find()
     return render_template('index.html', inputs=inputs)
 
 @app.route('/delete_database', methods=['GET', 'POST'])
@@ -35,18 +38,15 @@ def delete_database():
         return render_template('delete_confirm.html')
 
     elif request.method == 'POST':
-        # Delete all entries in the UserInput table
-        db.session.query(UserInput).delete()
-        db.session.commit()
+        # Delete all entries in the user_inputs collection
+        db.user_inputs.delete_many({})
 
         return redirect(url_for('index'))
-    
+
 @app.route('/show_database')
 def show_database():
-    inputs = UserInput.query.all()
+    inputs = db.user_inputs.find()
     return render_template('show_database.html', inputs=inputs)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
