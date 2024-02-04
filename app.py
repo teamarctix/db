2834ded6f2260262556983dfde54ec9285be2428
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-02-04 13:27:58 +0530

    add timestamp

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from datetime import datetime
+from bson.objectid import ObjectId
+
+import pytz
+
 
 app = Flask(__name__)
 client = MongoClient('mongodb+srv://abcd:abcd@personal.dzhmvck.mongodb.net/?retryWrites=true&w=majority')
 db = client['data_db']
 
 class UserInput:
-    def __init__(self, api_id, api_hash, bot_token, owner_id):
+    def __init__(self, api_id, api_hash, bot_token, owner_id, timestamp):
         self.api_id = api_id
         self.api_hash = api_hash
         self.bot_token = bot_token
         self.owner_id = owner_id
+        self.timestamp = timestamp
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -20,12 +26,17 @@ def index():
         bot_token = request.form['bot_token']
         owner_id = request.form['owner_id']
 
-        user_input = UserInput(api_id=api_id, api_hash=api_hash, bot_token=bot_token, owner_id=owner_id)
+        local_tz = pytz.timezone('Asia/Kolkata')  
+        timestamp = datetime.now(local_tz).strftime('%Y-%m-%d %H:%M:%S')
+
+        user_input = UserInput(api_id=api_id, api_hash=api_hash, bot_token=bot_token, owner_id=owner_id, timestamp=timestamp)
         db.user_inputs.insert_one({
+            # '_id': ObjectId(),
             'api_id': user_input.api_id,
             'api_hash': user_input.api_hash,
             'bot_token': user_input.bot_token,
-            'owner_id': user_input.owner_id
+            'owner_id': user_input.owner_id,
+            'timestamp': user_input.timestamp
         })
 
         # Redirect to avoid form resubmission on page refresh
@@ -45,11 +56,16 @@ def delete_database():
         db.user_inputs.delete_many({})
 
         return redirect(url_for('index'))
-
+    
 @app.route('/show_database')
 def show_database():
     inputs = db.user_inputs.find()
     return render_template('show_database.html', inputs=inputs)
+
+# @app.route('/delete_row/<id>', methods=['POST'])
+# def delete_row(id):
+#     db.user_inputs.delete_one({'_id': ObjectId(id)})
+#     return redirect(url_for('show_database'))
 
 if __name__ == '__main__':
     app.run(debug=True)
