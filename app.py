e556fb7b4508456544be7adec488f9cf21159eef
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-02-04 23:06:34 +0530

    add bot user name

--- app.py
+++ app.py
@@ -2,20 +2,26 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from datetime import datetime
 from bson import ObjectId
+import requests
+import os
 import pytz
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 app = Flask(__name__)
-client = MongoClient('mongodb+srv://abcd:abcd@personal.dzhmvck.mongodb.net/?retryWrites=true&w=majority')
-db = client['data_db']
+client = MongoClient(os.getenv('MONGO_CLIENT_LINK'))
+db = client[os.getenv('DB_NAME')]
 
 class UserInput:
-    def __init__(self, api_id, api_hash, bot_token, owner_id, timestamp):
+    def __init__(self, api_id, api_hash, bot_token, owner_id, timestamp, bot_name):
         self.api_id = api_id
         self.api_hash = api_hash
         self.bot_token = bot_token
         self.owner_id = owner_id
         self.timestamp = timestamp
+        self.bot_name = bot_name
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -25,16 +31,20 @@ def index():
         bot_token = request.form['bot_token']
         owner_id = request.form['owner_id']
 
+        bot_info_response = requests.get(f'https://api.telegram.org/bot{bot_token}/getMe')
+        bot_name = bot_info_response.json()['result']['username'] if bot_info_response.status_code == 200 else None
+
         local_tz = pytz.timezone('Asia/Kolkata')  
         timestamp = datetime.now(local_tz).strftime('%Y-%m-%d %H:%M:%S')
 
-        user_input = UserInput(api_id=api_id, api_hash=api_hash, bot_token=bot_token, owner_id=owner_id, timestamp=timestamp)
+        user_input = UserInput(api_id=api_id, api_hash=api_hash, bot_token=bot_token, owner_id=owner_id, timestamp=timestamp, bot_name=bot_name)
         db.user_inputs.insert_one({
             'api_id': user_input.api_id,
             'api_hash': user_input.api_hash,
             'bot_token': user_input.bot_token,
             'owner_id': user_input.owner_id,
-            'timestamp': user_input.timestamp
+            'timestamp': user_input.timestamp,
+            'bot_name': user_input.bot_name 
         })
 
         # Redirect to avoid form resubmission on page refresh
